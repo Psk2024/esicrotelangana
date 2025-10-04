aa02880269757a981df48f04259fe3817b27bc86
Sateesh Kumar Pikkili <163013785+Psk2024@users.noreply.github.com>
2025-10-04 07:49:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -113,8 +113,8 @@ function showEmployeeModal(index) {
   document.getElementById('modalTitle').textContent = emp[1] || '';
 
   const modalBody = document.getElementById('modalBody');
-  modalBody.innerHTML = `<table><tr><td>
-    <p><strong>Employee ID:</strong></td><td> ${highlight(emp[0]||'', searchTerm)}</p></td></tr></table>
+  modalBody.innerHTML = `<table>
+  <tr><td><p><strong>Employee ID:/p></strong></td><td><p> ${highlight(emp[0]||'', searchTerm)}</p></td></tr></table>
     <p><strong>Name:</strong> ${highlight(emp[1]||'', searchTerm)}</p>
     <p><strong>Designation:</strong> ${highlight(emp[2]||'', searchTerm)}</p>
     <p><strong>Branch:</strong> ${highlight(emp[7]||'', searchTerm)}</p>
