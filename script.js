a1118b2e1310eca36f7119d1fcb888bc594536fd
Sateesh Kumar Pikkili <163013785+Psk2024@users.noreply.github.com>
2026-04-01 07:54:28 +0530

    Update medical officer roles in script.js

--- script.js
+++ script.js
@@ -13,12 +13,12 @@ const DESIGNATION_ALIAS = {
   "Office Superintendent": "Social Security Officer",
 
   /* Insurance Medical Officer group */
-  "IMO Gr. I": "SMO/MVO/CMO/MR/IMO",
-  "IMO Gr. II": "SMO/MVO/CMO/MR/IMO",
-  "State Medical Officer":"SMO/MVO/CMO/MR/IMO",
-  "Medical Vigilance Officer":"SMO/MVO/CMO/MR/IMO",
-  "Chief Medical Officer":"SMO/MVO/CMO/MR/IMO",
-  "Medical Referee":"SMO/MVO/CMO/MR/IMO",
+  "IMO Gr. I": "Medical Officers",
+  "IMO Gr. II": "Medical Officers",
+  "State Medical Officer":"Medical Officers",
+  "Medical Vigilance Officer":"Medical Officers",
+  "Chief Medical Officer":"Medical Officers",
+  "Medical Referee":"Medical Officers",
   
   "Cook":"Canteen Staff",
   "Cleaner":"Canteen Staff",
